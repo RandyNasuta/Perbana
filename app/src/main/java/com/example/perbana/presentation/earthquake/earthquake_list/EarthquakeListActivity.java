7fae6353f6edb6ba5d654643a7828261bac33955
RandyNasuta <randynasuta123@gmail.com>
2026-08-08 21:08:23 +0700

    Pembuatan halaman gempa (08/08/2026)

---
+++ app/src/main/java/com/example/perbana/presentation/earthquake/earthquake_list/EarthquakeListActivity.java
@@ -0,0 +1,26 @@
+package com.example.perbana.presentation.earthquake.earthquake_list;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.perbana.R;
+
+public class EarthquakeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_earthquake_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
